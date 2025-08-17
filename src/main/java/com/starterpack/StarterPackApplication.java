7b07be4f53749a4433fccab9a873096fa18ee3dc
jihwan38 <sss203800@gmail.com>
2025-08-18 00:14:46 +0900

    feat: 초기 프로젝트 세팅

---
+++ src/main/java/com/starterpack/StarterPackApplication.java
@@ -0,0 +1,13 @@
+package com.starterpack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterPackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterPackApplication.class, args);
+	}
+
+}
