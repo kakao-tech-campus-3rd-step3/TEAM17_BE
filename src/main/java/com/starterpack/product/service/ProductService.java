d5316e85db94854443407cb8b6a465dc77ac5174
JIHWAN <sss203800@gmail.com>
2025-08-20 22:02:29 +0900

    상품 CRUD  (#7)
    * feat: 상품 CRUD
    
    * fix: ProductService에서 잘못된 reqeustDto를 Product를 생성하는 부분에서 생성자 순서가 잘못되어 있는 부분 수정

--- src/main/java/com/starterpack/product/service/ProductService.java
+++ src/main/java/com/starterpack/product/service/ProductService.java
@@ -2,22 +2,30 @@ package com.starterpack.product.service;
 
 import com.starterpack.category.entity.Category;
 import com.starterpack.category.repository.CategoryRepository;
+
+import com.starterpack.product.dto.ProductCreateRequestDto;
+import com.starterpack.product.dto.ProductDetailResponseDto;
+import com.starterpack.product.dto.ProductSimpleResponseDto;
+import com.starterpack.product.dto.ProductUpdateRequestDto;
 import com.starterpack.product.entity.Product;
 import com.starterpack.product.repository.ProductRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
 public class ProductService {
-
     private final ProductRepository productRepository;
     private final CategoryRepository categoryRepository;
 
-    @Transactional
+    public ProductService(ProductRepository productRepository, CategoryRepository categoryRepository) {
+        this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
     public Product saveProduct(Product product, Long categoryId) {
         if (product.getId() != null) {
             Product existingProduct = findProductById(product.getId());
@@ -39,10 +47,6 @@ public class ProductService {
         return productRepository.save(product);
     }
 
-    @Transactional
-    public void deleteProduct(Long id) {
-        productRepository.deleteById(id);
-    }
 
     public Product findProductById(Long id) {
         return productRepository.findById(id)
@@ -51,5 +55,72 @@ public class ProductService {
 
     public List<Product> findAllProducts() {
         return productRepository.findAll();
+    }
+
+    //여기서부터 시작
+    public ProductDetailResponseDto createProduct(ProductCreateRequestDto productCreateRequestDto) {
+        Category category = getCategoryByCategoryId(productCreateRequestDto.categoryId());
+
+        Product product = new Product(
+                productCreateRequestDto.name(),
+                productCreateRequestDto.link(),
+                productCreateRequestDto.productType(),
+                productCreateRequestDto.src(),
+                productCreateRequestDto.cost(),
+                category
+        );
+
+        return ProductDetailResponseDto.from(productRepository.save(product));
+    }
+
+    @Transactional(readOnly = true)
+    public ProductDetailResponseDto getProductDetail(Long productId) {
+        Product product = productRepository.findById(productId).
+                orElseThrow(() -> new IllegalArgumentException(productId + "에 해당하는 상품을 찾지 못했습니다."));
+
+        return ProductDetailResponseDto.from(product);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ProductSimpleResponseDto> getProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream()
+                .map(ProductSimpleResponseDto::from)
+                .collect(Collectors.toList());
+    }
+
+    public ProductDetailResponseDto updateProduct(Long productId, ProductUpdateRequestDto productUpdateRequestDto) {
+        Product product = getProduct(productId);
+
+        Category category = getCategoryByCategoryId(productUpdateRequestDto.categoryId());
+
+        product.update(
+                productUpdateRequestDto.name(),
+                productUpdateRequestDto.link(),
+                productUpdateRequestDto.productType(),
+                productUpdateRequestDto.src(),
+                productUpdateRequestDto.cost(),
+                category);
+
+        return ProductDetailResponseDto.from(productRepository.save(product));
+    }
+
+    public void deleteProduct(Long productId) {
+        if(!productRepository.existsById(productId)) {
+            throw new IllegalArgumentException(productId + "번에 해당하는 상품을 찾을 수 없습니다.");
+        }
+
+        productRepository.deleteById(productId);
+    }
+
+    private Category getCategoryByCategoryId(Long categoryId) {
+        return categoryRepository.findById(categoryId).
+                orElseThrow(() -> new IllegalArgumentException(categoryId + "번에 해당하는 카테고리를 찾지 못했습니다."));
+    }
+
+    private Product getProduct(Long productId) {
+        return productRepository.findById(productId)
+                .orElseThrow(() -> new IllegalArgumentException(productId + "번에 해당하는 상품을 찾지 못했습니다."));
     }
 }
