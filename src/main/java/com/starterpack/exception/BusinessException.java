175db1c4621c3d4afb6471da65d24b891cc6a48f
jihwan38 <sss203800@gmail.com>
2025-09-04 15:19:32 +0900

    feat: 비즈니스 로직 관련 예외를 위한 BusinessException 예외 클래스 추가

---
+++ src/main/java/com/starterpack/exception/BusinessException.java
@@ -0,0 +1,19 @@
+package com.starterpack.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getErrorMessage());
+        this.errorCode = errorCode;
+    }
+
+    public BusinessException(ErrorCode errorCode, String detailMessage) {
+        super(detailMessage);
+        this.errorCode = errorCode;
+    }
+}
