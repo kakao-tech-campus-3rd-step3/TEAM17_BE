c36d043126af8149f4e20a6b2ec15fd89f00df67
sanghyeon1225 <oopt1234@naver.com>
2025-09-12 09:30:59 +0900

    refactor: CD 중 에러나는 부분 해결

--- src/main/java/com/starterpack/config/SecurityConfig.java
+++ src/main/java/com/starterpack/config/SecurityConfig.java
@@ -30,7 +30,8 @@ public class SecurityConfig {
     private static final String[] PUBLIC_URLS = {
             "/", // 루트 경로
             "/api/auth/**", // 로그인, 회원가입 등 인증 관련 API
-            "/admin/**"
+            "/admin/**",
+            "/actuator/**"
     };
 
     @Bean
