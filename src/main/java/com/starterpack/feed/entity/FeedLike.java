18b19b2a1ebd7855e268d223d85f54c80220ccd0
jihwan38 <sss203800@gmail.com>
2025-09-17 23:41:29 +0900

    feat: 피드 좋아요 엔티티인 FeedLike 추가

---
+++ src/main/java/com/starterpack/feed/entity/FeedLike.java
@@ -0,0 +1,35 @@
+package com.starterpack.feed.entity;
+
+import com.starterpack.member.entity.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Table(name="feed_like")
+public class FeedLike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="feed_id", nullable=false)
+    private Feed feed;
+
+    @ManyToOne
+    @JoinColumn(name="member_id", nullable=false)
+    private Member member;
+
+    @CreationTimestamp
+    @Column(name="created_at", nullable=false)
+    private LocalDateTime createdAt;
+
+    protected FeedLike() {}
+}
