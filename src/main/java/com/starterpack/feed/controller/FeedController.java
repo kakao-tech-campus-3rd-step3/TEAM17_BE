3656e57d1f2b7cfaa2d66b3c75790a305e3a16db
jihwan38 <sss203800@gmail.com>
2025-09-19 00:15:59 +0900

    feat: Swagger 적용 및 로그인 어노테이션 적용

--- src/main/java/com/starterpack/feed/controller/FeedController.java
+++ src/main/java/com/starterpack/feed/controller/FeedController.java
@@ -1,6 +1,5 @@
 package com.starterpack.feed.controller;
 
-import com.starterpack.auth.CustomMemberDetails;
 import com.starterpack.auth.login.Login;
 import com.starterpack.feed.dto.FeedCreateRequestDto;
 import com.starterpack.feed.dto.FeedLikeResponseDto;
@@ -11,13 +10,13 @@ import com.starterpack.feed.entity.Feed;
 import com.starterpack.feed.service.FeedService;
 import com.starterpack.member.entity.Member;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,6 +36,8 @@ public class FeedController {
     }
 
     @PostMapping
+    @Operation(summary = "피드 생성", description = "새로운 피드를 생성합니다.")
+    @SecurityRequirement(name = "Bearer Authentication")
     public ResponseEntity<FeedResponseDto> addFeed (
             @Login Member member,
             @RequestBody FeedCreateRequestDto feedCreateDto
@@ -49,6 +50,7 @@ public class FeedController {
     }
 
     @GetMapping("/{feedId}")
+    @Operation(summary = "피드 상세 조회", description = "특정 피드의 상세 정보를 조회합니다.")
     public ResponseEntity<FeedResponseDto> getFeed (@PathVariable Long feedId) {
         Feed feed = feedService.getFeed(feedId);
 
@@ -58,6 +60,7 @@ public class FeedController {
     }
 
     @GetMapping
+    @Operation(summary = "피드 목록 조회", description = "모든 피드 목록을 페이지로 조회합니다.")
     public ResponseEntity<Page<FeedResponseDto>> getAllFeeds(
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
         Page<Feed> feedPage = feedService.getAllFeeds(pageable);
@@ -68,6 +71,8 @@ public class FeedController {
     }
 
     @PutMapping("/{feedId}")
+    @Operation(summary = "피드 수정", description = "기존 피드의 정보를 수정합니다.(상품 정보는 변경 불가)")
+    @SecurityRequirement(name = "Bearer Authentication")
     public ResponseEntity<FeedResponseDto> updateFeed(
             @PathVariable Long feedId,
             @Login Member member,
@@ -81,17 +86,20 @@ public class FeedController {
     }
 
     @DeleteMapping("/{feedId}")
+    @Operation(summary = "피드 삭제", description = "특정 피드를 삭제합니다.")
+    @SecurityRequirement(name = "Bearer Authentication")
     public ResponseEntity<Void> deleteFeed(
             @PathVariable Long feedId,
-            @AuthenticationPrincipal CustomMemberDetails customMemberDetails
+            @Login Member member
     ) {
-        feedService.deleteFeed(feedId, customMemberDetails.getMember());
+        feedService.deleteFeed(feedId, member);
 
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/{feedId}/like")
     @Operation(summary = "피드 좋아요 토글", description = "피드의 좋아요를 추가하거나 취소합니다.")
+    @SecurityRequirement(name = "Bearer Authentication")
     public ResponseEntity<FeedLikeResponseDto> toggleFeedLike(
             @PathVariable Long feedId,
             @Login Member member
