883d743777e786948ab5e801f1c64e8ffc2ceb56
sanghyeon1225 <oopt1234@naver.com>
2025-09-19 16:57:01 +0900

    refactor: 배포 서버도 CORS 추가

--- src/main/java/com/starterpack/config/WebConfig.java
+++ src/main/java/com/starterpack/config/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "https://team17be-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
