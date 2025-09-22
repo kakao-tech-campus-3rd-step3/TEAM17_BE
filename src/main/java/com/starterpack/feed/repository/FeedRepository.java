11d456451bcc9360319412ae83794af028c965aa
jihwan38 <sss203800@gmail.com>
2025-09-22 21:09:52 +0900

    refactor: 현재는 잘 작동하지만 이후 JpaRepository 인터페이스 상속으론 JpaSpecification이 작동 안할 수 있기 때문에 JpaSpecificationExecutor도 상속하도록 변경

--- src/main/java/com/starterpack/feed/repository/FeedRepository.java
+++ src/main/java/com/starterpack/feed/repository/FeedRepository.java
@@ -7,13 +7,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FeedRepository extends JpaRepository<Feed, Long> {
+public interface FeedRepository extends JpaRepository<Feed, Long>, JpaSpecificationExecutor<Feed> {
     @Query("SELECT f FROM Feed f " +
             "JOIN FETCH f.user " +
             "LEFT JOIN FETCH f.category " +
