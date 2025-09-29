64a006e452736a87c8667efad12ed1ba81c5325f
jihwan38 <sss203800@gmail.com>
2025-09-29 16:01:18 +0900

    feat: ErrorResponse에 List<FieldErrorDetail> 추가

--- src/main/java/com/starterpack/exception/ErrorResponse.java
+++ src/main/java/com/starterpack/exception/ErrorResponse.java
@@ -1,9 +1,27 @@
 package com.starterpack.exception;
 
-public record ErrorResponse(String errorCode, String errorMessage) {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.List;
 
-    public static ErrorResponse of(final ErrorCode errorCode) {
-        return new ErrorResponse(errorCode.getErrorCode(),
-                errorCode.getErrorMessage());
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ErrorResponse(
+        String errorCode,
+        String errorMessage,
+        List<FieldErrorDetail> errors
+) {
+    public static ErrorResponse of(ErrorCode errorCode) {
+        return new ErrorResponse(
+                errorCode.getErrorCode(),
+                errorCode.getErrorMessage(),
+                null
+        );
+    }
+
+    public static ErrorResponse of(ErrorCode errorCode, List<FieldErrorDetail> errors) {
+        return new ErrorResponse(
+                errorCode.getErrorCode(),
+                errorCode.getErrorMessage(),
+                errors
+        );
     }
 }
