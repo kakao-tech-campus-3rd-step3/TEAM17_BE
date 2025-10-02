e2d42d838fd5e546483dfcd85448d02a62120b65
JIHWAN <sss203800@gmail.com>
2025-10-02 15:14:04 +0900

    feat: Validation 검증 실패 시 발생하는 MethodArgumentNotValidException 처리 (#77)
    * feat: 링크 검사 서비스 추가
    
    * feat: 필드에 대한 상세 오류 정보를 담기 위한 FieldErrorDetail DTO 추가
    
    * feat: ErrorResponse에 List<FieldErrorDetail> 추가
    
    * feat: 전역예외처리기에 MethodArgumentNotValidException 처리 로직 추가
    
    * fix: Rebase 중 다시 생긴 파일 삭제
    
    ---------
    
    Co-authored-by: SeungWon <212329@jnu.ac.kr>

--- src/main/java/com/starterpack/exception/GlobalExceptionHandler.java
+++ src/main/java/com/starterpack/exception/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.starterpack.exception;
 
 
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,8 +19,22 @@ public class GlobalExceptionHandler {
 
         log.warn("Business Exception Occurred: {}", errorCode);
 
-        final ErrorResponse errorResponse = new ErrorResponse(errorCode.getErrorCode(),
-                e.getMessage());
+        final ErrorResponse errorResponse = ErrorResponse.of(errorCode);
+
+        return new ResponseEntity<>(errorResponse, errorCode.getHttpStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        final ErrorCode errorCode = ErrorCode.INVALID_INPUT_VALUE;
+
+        List<FieldErrorDetail> fieldErrors = e.getBindingResult().getFieldErrors().stream()
+                .map(FieldErrorDetail::from)
+                .toList();
+
+        log.warn("MethodArgumentNotValidException Occurred: {}", errorCode);
+
+        final ErrorResponse errorResponse = ErrorResponse.of(errorCode, fieldErrors);
 
         return new ResponseEntity<>(errorResponse, errorCode.getHttpStatus());
     }
