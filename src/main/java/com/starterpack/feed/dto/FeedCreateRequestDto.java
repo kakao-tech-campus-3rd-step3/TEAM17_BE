320dd6340c748533a6f303c99b27f26a6a183543
jihwan38 <sss203800@gmail.com>
2025-10-03 19:47:40 +0900

    refactor: 더 이상 쓰이지 않는 DTO 삭제 및 FeedCreateRequestDto 리팩토링

--- src/main/java/com/starterpack/feed/dto/FeedCreateRequestDto.java
+++ src/main/java/com/starterpack/feed/dto/FeedCreateRequestDto.java
@@ -1,16 +1,8 @@
 package com.starterpack.feed.dto;
 
-import com.starterpack.feed.entity.FeedType;
-import java.util.List;
-
 public record FeedCreateRequestDto(
     String description,
     String imageUrl,
-    FeedType feedType,
-    Long categoryId,
-    List<ProductTagRequestDto> products
+    Long categoryId
 ) {
-    public boolean isInfoFeedWithProducts() {
-        return this.feedType() == FeedType.INFO && this.products() != null && !this.products().isEmpty();
-    }
 }
