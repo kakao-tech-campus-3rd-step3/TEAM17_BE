8593a739ebb667be4ad12dd52b1de4a1b4126ca3
SeungWon <212329@jnu.ac.kr>
2025-10-03 22:21:46 +0900

    refactor: 카테고리 조회 응답결과에서 src 제거

--- src/main/java/com/starterpack/category/dto/CategoryResponseDto.java
+++ src/main/java/com/starterpack/category/dto/CategoryResponseDto.java
@@ -7,11 +7,9 @@ import lombok.Getter;
 public class CategoryResponseDto {
     private Long id;
     private String name;
-    private String src;
 
     public CategoryResponseDto(Category category) {
         this.id = category.getId();
         this.name = category.getName();
-        this.src = category.getSrc();
     }
 }
