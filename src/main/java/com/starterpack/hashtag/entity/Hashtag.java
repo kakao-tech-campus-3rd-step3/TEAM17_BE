1723a76ff328353d03a17ca190a117e25941d2d1
jihwan38 <sss203800@gmail.com>
2025-10-08 03:06:17 +0900

    feat: 해시태그 엔티티 구현 및 H2 DB 스키마에 테이블 추가

---
+++ src/main/java/com/starterpack/hashtag/entity/Hashtag.java
@@ -0,0 +1,43 @@
+package com.starterpack.hashtag.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "hashtag")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Hashtag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "usage_count", nullable = false)
+    private Long usageCount = 0L;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    public Hashtag(String name) {
+        this.name = name;
+    }
+}
